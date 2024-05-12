3af3b29cf9b6adcced25d4bb6e781625e4bc0e5c
Swartex <andrey92.ua@gmail.com>
2024-05-13 00:44:54 +0300

    test commit

--- src/components/ui/Button.test.tsx
+++ src/components/ui/Button.test.tsx
@@ -1,14 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Button } from "./button";
 
 describe("Button", () => {
   it("renders", () => {
     const { asFragment } = render(<Button>test</Button>);
+
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders ghost button", () => {
-    const { asFragment } = render(<Button variant="ghost">test</Button>);
+    const { asFragment} = render(<Button variant="ghost">test1</Button>);
+    const button = screen.getByRole('button');
+
+    expect(button).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('render small button', () => {
+    render(<Button size="sm" variant="destructive">Button text</Button>);
+    const button = screen.findByRole('button');
+
+    waitFor(() => {
+      console.log(button)
+    })
+  })
 });
